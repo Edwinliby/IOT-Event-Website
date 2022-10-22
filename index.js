ae868eeca336a9de78819778546df43bf506d63d
Edwinliby <edwinliby30@gmail.com>
2022-10-22 18:33:14 +0530

    navbar added

---
+++ index.js
@@ -0,0 +1,18 @@
+const navsilde = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link) => {
+      if (link.style.animation) 
+        link.style.animation = "";
+      else 
+        link.style.animation = "navLinkFade 0.5s ease forwards .75s";
+    });
+  });
+};
+
+navsilde();
